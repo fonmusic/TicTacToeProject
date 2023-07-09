ec1e09da602cb34195529fa657d8e0112cdc5250
fonmusic <red-strat@mail.ru>
2023-07-10 00:29:42 +0300

    API project recreated + sqlite added

---
+++ TicTacToeAPI/Dtos/MoveDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TicTacToeAPI.Dtos;
+
+public class MoveDto
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+}
---
+++ TicTacToeAPI/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using TicTacToeAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<TicTacToeDbContext>
+    (opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
