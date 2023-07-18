6d73c68400f0ea51770e5485c52b12e16721781b
fonmusic <red-strat@mail.ru>
2023-07-18 04:22:47 +0300

    GameController added

---
+++ TicTacToeAPI/Controllers/GameController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicTacToeApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GameController : ControllerBase
+{
+    private readonly IGameService _gameService;
+    public GameController(IGameService gameService)
+    {
+        this._gameService = gameService;
+    }
+
+    [HttpGet("GetAllGames")]
+    public async Task<ActionResult<ServiceResponse<List<GetGameDto>>>> GetAllGames()
+    {
+        var result = _gameService.GetAllGames();
+        return Ok(await result);
+    }
+
+    [HttpGet("GetGameById/{id}")]
+    public async Task<ActionResult<ServiceResponse<GetGameDto>>> GetGameById(int id)
+    {
+        var result = _gameService.GetGameById(id);
+        if (result is null)
+            return NotFound();
+
+        return Ok(await result);
+    }
+
+    [HttpPost("StartNewGame")]
+    public async Task<ActionResult<ServiceResponse<GetGameDto>>> StartNewGame()
+    {
+        var newGame = new StartNewGameDto();
+        var result = _gameService.StartNewGame(newGame);
+        return Ok(await result);
+    }
+
+    [HttpPut("UpdateGame")]
+    public async Task<ActionResult<ServiceResponse<GetGameDto>>> UpdateGame(UpdateGameDto updatedGame)
+    {
+        var result = _gameService.UpdateGame(updatedGame);
+        if (result is null)
+            return NotFound();
+
+        return Ok(await result);
+    }
+}
