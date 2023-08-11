28291f531de8f0882896d0519c4f75fe47e9033b
fonmusic <red-strat@mail.ru>
2023-08-12 01:52:14 +0300

    Bootstrap package installed

--- TicTacToeAPI/Dtos/Game/GetGameDto.cs
+++ TicTacToeAPI/Dtos/Game/GetGameDto.cs
@@ -4,8 +4,4 @@ public class GetGameDto
 {
     public int Id { get; set; }
     public TicTacToeDescription Description { get; set; } = new();
-    // public required string Board { get; set; }
-    // public string NextPlayer { get; set; } = string.Empty;
-    // public string Winner { get; set; } = string.Empty;
-    // public TicTacToeGameState GameState { get; set; }
 }
