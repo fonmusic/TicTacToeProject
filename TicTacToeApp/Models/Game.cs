4cbe5391f2ad80cd534b54d8abf2ef02e0f26361
fonmusic <red-strat@mail.ru>
2023-08-11 18:29:46 +0300

    Add Game & ServiceResponse models

---
+++ TicTacToeApp/Models/Game.cs
@@ -0,0 +1,12 @@
+using TicTacToeLibrary;
+
+namespace TicTacToeApp.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+    public required string Board { get; set; }
+    public required string NextPlayer { get; set; }
+    public required string Winner { get; set; }
+    public TicTacToeGameState GameState { get; set; }
+}
