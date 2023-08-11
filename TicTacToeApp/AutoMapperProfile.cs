2ec0be2617a6c7352d26162b14a5f18b4d093599
fonmusic <red-strat@mail.ru>
2023-08-11 18:46:08 +0300

    AutoMapperProfile added

---
+++ TicTacToeApp/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+namespace TicTacToeApp;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<Game, GetGameDto>()
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src =>
+                new TicTacToeDescription
+                {
+                    Board = src.Board,
+                    NextPlayer = src.NextPlayer,
+                    Winner = src.Winner,
+                    GameState =     src.GameState
+                }));
+        CreateMap<StartNewGameDto, Game>();
+        CreateMap<UpdateGameDto, Game>();
+        CreateMap<Game, TicTacToeDescription>();
+        CreateMap<TicTacToeDescription, Game>();
+    }    
+}
--- TicTacToeApp/GlobalUsings.cs
+++ TicTacToeApp/GlobalUsings.cs
@@ -1,3 +1,6 @@
 // Global using directives
 
+global using AutoMapper;
+global using TicTacToeApp.Dtos;
+global using TicTacToeApp.Models;
 global using TicTacToeLibrary;
