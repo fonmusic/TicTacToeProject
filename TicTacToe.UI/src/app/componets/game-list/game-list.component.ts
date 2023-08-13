3647d4aef833ea1bc43174fe7708657b8fd82c5d
fonmusic <red-strat@mail.ru>
2023-08-13 03:26:16 +0300

    Edit game-list.components

--- TicTacToe.UI/src/app/componets/game-list/game-list.component.ts
+++ TicTacToe.UI/src/app/componets/game-list/game-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {GameService} from "../../services/game.service";
+import {GetGameDto} from "../../models/getGameDto";
 
 @Component({
   selector: 'app-game-list',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent {
+  constructor(private gameService: GameService) { }
 
+  games: GetGameDto[] = [];
+
+  ngOnInit(): void {
+    this.getAllGames();
+  }
+
+  getAllGames() {
+    this.gameService.getAllGames().subscribe(response => {
+      if (response.success) {
+        this.games = response.data;
+      }
+    });
+  }
 }
--- TicTacToe.UI/src/app/services/game.service.ts
+++ TicTacToe.UI/src/app/services/game.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {UpdateGameDto} from "../models/updateGameDto";
+import {Observable} from "rxjs";
+import {ServiceResponse} from "../models/serviceResponse";
+import {GetGameDto} from "../models/getGameDto";
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +13,19 @@ export class GameService {
   private apiUrl = `${environment.apiUrl}/Game`
   constructor(private http: HttpClient) { }
 
-  getAllGames() {
-    return this.http.get(`${this.apiUrl}/GetAllGames`);
+  getAllGames() : Observable<ServiceResponse<GetGameDto[]>> {
+    return this.http.get<ServiceResponse<GetGameDto[]>>(`${this.apiUrl}/GetAllGames`);
   }
 
-  getGameById(id: number) {
-    return this.http.get(`${this.apiUrl}/GetGameById/${id}`);
+  getGameById(id: number) : Observable<ServiceResponse<GetGameDto>> {
+    return this.http.get<ServiceResponse<GetGameDto>>(`${this.apiUrl}/GetGameById/${id}`);
   }
 
-  startNewGame() {
-    return this.http.post(`${this.apiUrl}/StartNewGame`, null);
+  startGame() : Observable<ServiceResponse<GetGameDto>> {
+    return this.http.post<ServiceResponse<GetGameDto>>(`${this.apiUrl}/StartNewGame`, {});
   }
 
-  updateGame(updatedGame: UpdateGameDto) {
-    return this.http.put(`${this.apiUrl}/UpdateGame`, updatedGame);
+  updateGame(updateGameDto: UpdateGameDto) : Observable<ServiceResponse<GetGameDto>> {
+    return this.http.put<ServiceResponse<GetGameDto>>(`${this.apiUrl}/UpdateGame`, updateGameDto);
   }
 }
